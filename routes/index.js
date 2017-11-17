db06bec59024226024cbad01fe1ea2f69d214cc5
luohuacheng <154224099@qq.com>
2017-11-17 18:00:55 +0800

    修改路由

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
-import Topics from '../controller/topics'
+import Topics from '../controller/topicsCtrl'
+import User from '../controller/userCtrl'
 
 export default app => {
   app.get('/', Topics.getAllTopics)
   app.post('/topic', Topics.postOneTopics)
+  app.get('/user/:name', User.getUser)
+  app.post('/user', User.register)
+  app.post('/login', User.login)
 }
